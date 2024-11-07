8fa269cce40cfaee9b59207915730248de449780
yeseul <mpjeong0325@gmail.com>
2024-11-07 20:10:58 +0900

    supabase연결

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,10 +1,27 @@
 import {json} from '@sveltejs/kit';
 // import conn from '$lib/db'
-import conn from '../../lib/db'
+// import conn from '../../lib/db'
+
+// export async function GET() {
+//     const result = await conn.query('SELECT * FROM todos;');
+//     // console.log('result', result);
+   
+//     return json(result[0])
+// }
+
+// import { supabase } from '$lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 
 export async function GET() {
-    const result = await conn.query('SELECT * FROM todos;');
-    // console.log('result', result);
-   
-    return json(result[0])
-}
+  const { data, error } = await supabase
+    .from('todos')
+    .select('*');
+
+  if (error) {
+    console.error('Error fetching data:', error);
+    return json({ error: 'Error fetching data' });
+  } else {
+    console.log('Data:', data);
+    return json(data);
+  }
+  }
--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -4,9 +4,22 @@ export const todoData = writable([]);
 export const categories = writable(['추가', '기타']);
 
 // 초기 데이터 로드
+// const fetchTodoData = async () => {
+// 	try {
+// 		const response = await fetch('/api');
+// 		const data = await response.json();
+// 		todoData.set(data);
+// 	} catch (error) {
+// 		console.error('할 일 데이터를 가져오는데 실패했습니다:', error);
+// 	}
+// };
+
 const fetchTodoData = async () => {
 	try {
 		const response = await fetch('/api');
+		if (!response.ok) {
+			throw new Error('할 일 데이터를 가져오는데 실패했습니다.');
+		}
 		const data = await response.json();
 		todoData.set(data);
 	} catch (error) {
