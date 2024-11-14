114b3329a262a69ea27626dfdf2129aba8a99b86
yeseul <mpjeong0325@gmail.com>
2024-11-14 14:56:36 +0900

    env경로 변경

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wcfwglecbwhcsffjjkrj.supabase.co';
-const supabaseKey = import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// const supabaseKey = import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = process.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
 	console.error('Supabase 환경 변수가 설정되지 않았습니다.');
--- src/lib/supabaseServer.js
+++ src/lib/supabaseServer.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wcfwglecbwhcsffjjkrj.supabase.co';
-const supabaseServiceKey = import.meta.env.VITE_SERVICE_KEY;
+const supabaseServiceKey = process.env.VITE_SERVICE_KEY;
+// const supabaseServiceKey = import.meta.env.VITE_SERVICE_KEY;
 
 if (!supabaseUrl || !supabaseServiceKey) {
 	console.error('Supabase 서버 환경 변수가 설정되지 않았습니다.');
