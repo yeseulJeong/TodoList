abe541f50b325e3b91757eaccd5dfdbe0e65bf07
yeseul <mpjeong0325@gmail.com>
2024-11-27 19:52:11 +0900

    로그인죽임

--- src/routes/signup/+page.server.ts
+++ src/routes/signup/+page.server.ts
@@ -3,18 +3,18 @@ import { json } from '@sveltejs/kit';
 import supabaseServer from '$lib/supabaseServer';
 
 export const actions = {
-	default: async ({ cookies, request }) => {
-		console.log('cookies', cookies);
-		console.log('request', request);
-		const data = await request.formData();
-		const name = data.get('name');
-		const email = data.get('email');
-		const password = data.get('password');
+	// default: async ({ cookies, request }) => {
+	// 	console.log('cookies', cookies);
+	// 	console.log('request', request);
+	// 	const data = await request.formData();
+	// 	const name = data.get('name');
+	// 	const email = data.get('email');
+	// 	const password = data.get('password');
 
-		const{data, error} = await supabaseServer
-		.from('users')
-		.insert([{name, email, password}]);
+	// 	const{data, error} = await supabaseServer
+	// 	.from('users')
+	// 	.insert([{name, email, password}]);
 
-		return { success: true, email, password };
-	},
+	// 	return { success: true, email, password };
+	// },
 };
